e587dfbf8c3a206f97bf87589ba7d175a7081a60
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2023-12-23 14:35:00 +0200

    Handling Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +26,12 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (Throwable $e, Request $request) {
+            return response()->json([
+                'Error' => 'Invalid Creds'
+            ], 400);
         });
     }
 }
